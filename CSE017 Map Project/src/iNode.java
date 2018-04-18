fd851fa6fd83498f90bd2a6388a757d367f79b7c
CrystalLove221 <thogue314271@protonmail.com>
2018-04-18 13:20:12 -0400

    Corrected iNode (I hope)

--- CSE017 Map Project/src/iNode.java
+++ CSE017 Map Project/src/iNode.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 
 /**
  * @author Evan Hruskar
@@ -8,43 +6,44 @@
  * @version 2018.04.16
  *
  */
-public interface iNode<T> {
+public interface iNode<E> {
 
     /**
      * @return The location of the node (x,y)
      */
-    public Point getLocation();
+    public E getLocation();
     
     /**
      * Set the location of the node container
      * 
      * @param location The new (x,y) location for the node 
      */
-    public void setLocation(Point location);
+    public void setLocation(E location);
     
-    /*
+    /**
      * @return the names of places in an array list of strings
      */
-    public String[] getPlaces();
+    public ArrayList<E> getPlaces();
     
-    /*
+    /**
      * Insert list of places
      * @param list The list of places to be added
      */
-    public void setPlaces(String[] list);
+    public void setPlaces(ArrayList<E> list);
     
-    /*
+    /**
      * @return information about the node
      */
-    public T getData();
+     
+    public E getData();
     
-    /*
+    /**
      * Stores new data in the node at Point
      * @param data The data to be added to the node
      */
-    public void setData(T data);
+    public void setData(E data);
     
-    /*
+    /**
      * @return String representation of the data and places in
      * the node
      */
