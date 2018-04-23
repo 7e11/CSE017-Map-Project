a1b4bdf43c1d8ea7d7f57bfe5a893812a0280e4d
Evan Hruskar <hruskar.evan@gmail.com>
2018-04-23 14:34:25 -0400

    removed unused variable node2

--- CSE017 Map Project/src/NodeTest.java
+++ CSE017 Map Project/src/NodeTest.java
@@ -9,8 +9,6 @@ import student.TestCase;
 public class NodeTest extends TestCase {
     private Node<Point> node1;
     private ArrayList<String> list1;
-    
-    private Node<Point> node2;
     private ArrayList<String> list2;
     
     /**
@@ -20,7 +18,6 @@ public class NodeTest extends TestCase {
         list1 = new ArrayList<String>();
         list2 = new ArrayList<String>();
         node1 = new Node<Point>(new Point(1, 1), list1);
-        node2 = new Node<Point>(new Point(2, 2), list2);
     }
     
     /**
