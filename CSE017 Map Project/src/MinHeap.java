0bde559a029e6dfc00e4de9b60ac2550bc2d604a
Evan Hruskar <hruskar.evan@gmail.com>
2018-05-03 16:38:55 -0400

    MinHeap supports generic ArrayList input

--- CSE017 Map Project/src/MinHeap.java
+++ CSE017 Map Project/src/MinHeap.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -11,7 +13,7 @@
 
 public class MinHeap<E extends Comparable<E>> {
 
-    private E[] Heap; // Pointer to the heap array
+    private ArrayList<E> Heap; // Pointer to the heap array
     private int size; // Maximum size of the heap
     private int n;    // Number of things now in heap
 
@@ -22,9 +24,8 @@ public class MinHeap<E extends Comparable<E>> {
      * @param num
      * @param max
      */
-    @SuppressWarnings("unchecked")
-    public MinHeap(Object[] h, int num, int max) {
-        Heap = (E[]) h;
+    public MinHeap(ArrayList<E> h, int num, int max) {
+        Heap = h;
         n = num;
         size = max;
         
@@ -88,9 +89,9 @@ public class MinHeap<E extends Comparable<E>> {
             return;
         }
         int curr = n++;
-        Heap[curr] = key; // Start at end of heap
+        Heap.set(curr, key); // Start at end of heap
         // Now sift up until curr's parent's key > curr's key
-        while ((curr != 0) && (Heap[curr].compareTo(Heap[parent(curr)]) < 0)) {
+        while ((curr != 0) && (Heap.get(curr).compareTo(Heap.get(parent(curr))) < 0)) {
             swap(Heap, curr, parent(curr));
             curr = parent(curr);
         }
@@ -117,9 +118,9 @@ public class MinHeap<E extends Comparable<E>> {
         while (!isLeaf(pos)) {
             int j = leftchild(pos);
 
-            if ((j < (n - 1)) && (Heap[j].compareTo(Heap[j + 1]) > 0))
+            if ((j < (n - 1)) && (Heap.get(j).compareTo(Heap.get(j + 1)) > 0))
                 j++; // j is now index of child with greater value
-            if (Heap[pos].compareTo(Heap[j]) <= 0)
+            if (Heap.get(pos).compareTo(Heap.get(j)) <= 0)
                 return;
 
             swap(Heap, pos, j);
@@ -137,7 +138,7 @@ public class MinHeap<E extends Comparable<E>> {
         swap(Heap, 0, --n); // Swap maximum with last value
         if (n != 0) // Not on last element
             siftdown(0); // Put new heap root val in correct place
-        return Heap[n];
+        return Heap.get(n);
     }
     
     /**
@@ -153,14 +154,14 @@ public class MinHeap<E extends Comparable<E>> {
         else {
             swap(Heap, pos, --n); // Swap with last value
             // If we just swapped in a big value, push it up
-            while ((pos > 0) && (Heap[pos].compareTo(Heap[parent(pos)]) < 0)) {
+            while ((pos > 0) && (Heap.get(pos).compareTo(Heap.get(parent(pos))) < 0)) {
                 swap(Heap, pos, parent(pos));
                 pos = parent(pos);
             }
             if (n != 0)
                 siftdown(pos); // If it is little, push down
         }
-        return Heap[n];
+        return Heap.get(n);
     }
 
     /**
@@ -169,9 +170,9 @@ public class MinHeap<E extends Comparable<E>> {
      * @param i the first index to swap
      * @param j the second index to swap
      */
-    private void swap(Object[] arr, int i, int j) {
-        Object o = arr[i];
-        arr[i] = arr[j];
-        arr[j] = o;
+    private void swap(ArrayList<E> arr, int i, int j) {
+        E o = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, o);
     }
 }
