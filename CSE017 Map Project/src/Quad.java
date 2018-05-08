62016600b6a3b5e330a7b3a5783db98767d081a6
Evan Hruskar <hruskar.evan@gmail.com>
2018-05-08 00:03:13 -0400

    Reverted method name changes

--- CSE017 Map Project/src/Quad.java
+++ CSE017 Map Project/src/Quad.java
@@ -283,7 +283,7 @@ public class Quad implements iQuad {
             return new ArrayList<Node<Point>>();
             //did this instead of returning null to make next methods easier.
         }
-        return bst.find(new StreetNodes(streetName)).getLocationStreet();
+        return bst.find(new StreetNodes(streetName)).getLocations();
     }
     
     /**
